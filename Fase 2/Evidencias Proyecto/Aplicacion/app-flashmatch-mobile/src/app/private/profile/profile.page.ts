c113dcc5709f98b02cf31f40306d0d8e3bfc0dcd
dgonzalezch <daj.gonzalezch@gmail.com>
2024-09-16 01:58:11 -0300

    (FEAT): se crea vista base para perfil de usuario

--- Fase 2/Evidencias Proyecto/Aplicacion/app-flashmatch-mobile/src/app/private/profile/profile.page.ts
+++ Fase 2/Evidencias Proyecto/Aplicacion/app-flashmatch-mobile/src/app/private/profile/profile.page.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonSegment, IonSegmentButton, IonLabel, IonCardTitle, IonCardHeader, IonCard, IonCardContent, IonGrid, IonRow, IonCol, IonImg, IonIcon, IonItem, IonList, IonItemDivider, IonText, IonInput, IonFooter, IonButton, IonInputPasswordToggle } from '@ionic/angular/standalone';
 import { HeaderComponent } from 'src/app/shared/components/header/header.component';
 
 @Component({
@@ -9,13 +9,18 @@ import { HeaderComponent } from 'src/app/shared/components/header/header.compone
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, HeaderComponent]
+  imports: [IonButton, IonFooter, IonInput, IonText, IonItemDivider, IonList, IonItem, IonIcon, IonImg, IonCol, IonRow, IonGrid, IonCardContent, IonCard, IonCardHeader, IonCardTitle, IonLabel, IonSegmentButton, IonSegment, IonContent, IonHeader, IonTitle, IonToolbar, IonInputPasswordToggle, CommonModule, FormsModule, HeaderComponent]
 })
 export default class ProfilePage implements OnInit {
+
+  selectedSegment = signal<'data' | 'password'>('data');
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSegmentChange(event: CustomEvent) {
+    this.selectedSegment.set(event.detail.value);
+  }
 }
