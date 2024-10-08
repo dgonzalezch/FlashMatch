1792363c24eed70498a799d78a466215f3d2e7e9
dgonzalezch <daj.gonzalezch@gmail.com>
2024-10-08 01:12:07 -0300

    (FEAT): Se añade segment para separar las vistas de las canchas entre grid o list

--- Fase 2/Evidencias Proyecto/Aplicacion/app-flashmatch-mobile/src/app/private/partidos/create-partido/step-2/step-2.page.ts
+++ Fase 2/Evidencias Proyecto/Aplicacion/app-flashmatch-mobile/src/app/private/partidos/create-partido/step-2/step-2.page.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton, IonCard, IonGrid, IonRow, IonCol, IonText, IonCardContent, IonIcon, IonButton, IonFooter, IonLabel, IonItem, IonList, IonToggle, IonThumbnail, IonDatetimeButton, IonModal, IonDatetime, IonAccordion, IonAccordionGroup } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton, IonCard, IonGrid, IonRow, IonCol, IonText, IonCardContent, IonIcon, IonButton, IonFooter, IonLabel, IonItem, IonList, IonToggle, IonThumbnail, IonDatetimeButton, IonModal, IonDatetime, IonAccordion, IonAccordionGroup, IonCardHeader, IonCardTitle, IonCardSubtitle, IonSegment, IonSegmentButton } from '@ionic/angular/standalone';
 import { RouterLink } from '@angular/router';
 
 // Definición de la interfaz Cancha
@@ -22,9 +22,11 @@ interface Cancha {
   templateUrl: './step-2.page.html',
   styleUrls: ['./step-2.page.scss'],
   standalone: true,
-  imports: [IonAccordionGroup, IonAccordion, IonDatetime, IonModal, IonDatetimeButton, IonList, IonItem, IonLabel, IonFooter, IonButton, IonIcon, IonCardContent, IonText, IonCol, IonRow, IonGrid, IonCard, IonBackButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, RouterLink, IonToggle, IonThumbnail]
+  imports: [IonSegmentButton, IonSegment, IonCardSubtitle, IonCardTitle, IonCardHeader, IonAccordionGroup, IonAccordion, IonDatetime, IonModal, IonDatetimeButton, IonList, IonItem, IonLabel, IonFooter, IonButton, IonIcon, IonCardContent, IonText, IonCol, IonRow, IonGrid, IonCard, IonBackButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, RouterLink, IonToggle, IonThumbnail]
 })
 export default class Step2Page implements OnInit {
+
+  selectedSegment = signal<string>('list');
 
   // Lista de canchas
   canchas: Cancha[] = [
@@ -54,7 +56,6 @@ export default class Step2Page implements OnInit {
 
   useCurrentLocationValue: boolean = false; // Controla el uso de la ubicación actual
   selectedLocation: string | null = null;
-  constructor() { }
 
   ngOnInit() {
   }
@@ -85,4 +86,7 @@ export default class Step2Page implements OnInit {
     }
   }
 
+  segmentChanged(event: any) {
+    this.selectedSegment.set(event.detail.value);
+  }
 }
--- Fase 2/Evidencias Proyecto/Aplicacion/app-flashmatch-mobile/src/app/shared/components/menu/menu.component.ts
+++ Fase 2/Evidencias Proyecto/Aplicacion/app-flashmatch-mobile/src/app/shared/components/menu/menu.component.ts
@@ -17,11 +17,11 @@ export class MenuComponent implements OnInit {
   menuItems = signal([
     { icon: 'home-outline', label: 'Inicio', route: '/private/home' },
     { icon: 'football-outline', label: 'Partidos', route: '/private/matches' },
-    { icon: 'albums-outline', label: 'Canchas', route: '/private/courts' },
+    // { icon: 'albums-outline', label: 'Canchas', route: '/private/courts' },
     { icon: 'people-circle-outline', label: 'Equipos', route: '/private/teams' },
     { icon: 'walk-outline', label: 'Jugadores', route: '/private/players' },
     { icon: 'person-outline', label: 'Perfil', route: '/private/profile' },
-    { icon: 'settings-outline', label: 'Configuraciones', route: '/private/configurations' },
+    // { icon: 'settings-outline', label: 'Configuraciones', route: '/private/configurations' },
     // { icon: 'log-out-outline', label: 'Cerrar sesión', route: '/auth' },
     // { icon: 'help-circle-outline', label: 'Ayuda', route: '/help' }
   ]);
