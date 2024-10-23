4be87df5326a402bc7d34bd79e00e913d4f950b4
unknown <rod.berrios@duocuc.cl>
2024-10-23 00:25:49 -0300

    Descripción breve de los cambios

--- Fase 2/Evidencias Proyecto/Evidencias de sistema/Aplicacion/app-flashmatch-web/src/app/pages/about/about.component.ts
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/Aplicacion/app-flashmatch-web/src/app/pages/about/about.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faAndroid } from '@fortawesome/free-brands-svg-icons';
+
 
 @Component({
   selector: 'app-about',
@@ -8,5 +11,5 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
-
+  faAndroid = faAndroid;
 }
